91f09dff4900b5028209df195d626d0566f1656c
olarewajuakeemope <olarewajuakeemopeyemi@gmail.com>
2017-01-18 19:01:59 +0100

    completed user and admin dashboard routing

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ var express = require('express');
 var path = require('path');
 var firebase = require('firebase');
 var bodyParser = require('body-parser');
-var exphbs = require('express-handlebars');
-const adminUID = 's9uc9dexFJXjEnmX3X52jY1YlG12';
+const adminID = 's9uc9dexFJXjEnmX3X52jY1YlG12';
 var currID = '';
 
 var app = express();
@@ -18,12 +17,6 @@ var config = {   apiKey: "AIzaSyB89UfsZjSnvda76EdN4qwsjBfhvcqia78",   authDo
 firebase.initializeApp(config);
 
 const auth = firebase.auth();
-
-app.engine('handlebars', exphbs({ extname: 'handlebars', defaultLayout: 'main', layoutsDir: __dirname + '/views/' }));
-app.set('view engine', 'handlebars');
-app.get('/dashboard', function(req, res) {
-  res.render('home');
-});
 
 app.use('/cssFiles', express.static(__dirname + '/css'));
 app.use('/images', express.static(__dirname + '/img'));
@@ -53,9 +46,9 @@ app.post('/forms/userprocess', function(req, res) {
     }).then(function(data) {
       console.log('data');
       firebase.auth().onAuthStateChanged((user) => {
-        if (user) {
-          console.log(user.uid);
-        }
+        if(user){
+          console.log(user.uid);     
+    }
       });
       res.json({});
     });
@@ -79,19 +72,35 @@ app.post('/forms/loginprocess', function(req, res) {
     }).then(function(data) {
       console.log('data');
       firebase.auth().onAuthStateChanged((user) => {
-        if (user) {
+        if(user){
+          var adminUrl = '/dashboard';
+          if (user.uid === adminID) {
+          adminUrl = '/';
           console.log(user.uid);
         }
+        res.json({url: adminUrl});
+    }
       });
-      res.json({});
+      
     });
 })
+
+//my template engine begins
+
+
+app.set('view engine', 'ejs');
+
+app.get('/dashboard', function(req, res) {
+    res.render('pages/index');
+});
+
+//my template engine ends
+
 
 app.get('/forms/logout', function(req, res) {
   firebase.auth().signOut();
   res.json({});
 })
-
 
 router.get('/loginForm', function(req, res) {
   res.sendFile('loginForm.html', { root: path.join(__dirname, './files') });
--- js/auth.js
+++ js/auth.js
@@ -5,27 +5,27 @@ $(function(){
         e.preventDefault();
         $("#result").html('');
         var data = {'email' : $('input[name=userEmail]').val(), 'pass' : $('input[name=userPassword]').val()};
+        var url = "";
         $.ajax({
             url: "../forms/loginprocess",
             type: "post",
             dataType : "json",
             data: data,
             success: function(data) {
-                
                 if(data.error){
-                  console.log('inside the success if');
+                  //console.log('inside the success if');
                   console.log(data.error);
-                  $("#result").html(data.error.message);
+                  $("#result").html('data.error.message');
                   $('#progress').html('');
                 }else{
-                    console.log('inside the windows');
-                    window.location.href= "/dashboard";
+                    //console.log('inside the windows');
+                    window.location.href= data.url;
 
                 }
                 
             },
             error: function(data, error) {
-      console.log('inside the error');    
+      //console.log('inside the error');    
       $("#result").html(' ' + error);
       console.log(typeof error);
       $('#progress').html('');
